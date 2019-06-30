362677f812f3c0f1be370519773de0e90483aed0
Simon Mai <maismin@gmail.com>
2019-06-30 14:22:40 -0400

    Refactor app from index

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,18 +1,10 @@
-const config = require('./utils/config')
+
 const http = require('http')
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-const mongoose = require('mongoose')
+const app = require('./app')
+const config = require('./utils/config')
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+const server = http.createServer(app)
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use('/api/blogs', blogsRouter)
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
 })
