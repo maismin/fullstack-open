cbef92e2f42867ad97bfba9ac7d01f0aa0159e8d
Simon Mai <maismin@gmail.com>
2019-07-03 13:14:19 -0400

    Modify adding new blogs so that it is only possible with a valid token to HTTP POST request

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -1,5 +1,15 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
+const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response, next) => {
   try {
@@ -13,16 +23,37 @@ blogsRouter.get('/', async (request, response, next) => {
 })
 
 blogsRouter.post('/', async (request, response, next) => {
+  const body = request.body
+
+  const token = getTokenFrom(request)
+
   try {
-    const blog = new Blog(request.body)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({
+        error: 'token missing or invalid'
+      })
+    }
+    const user = await User.findById(decodedToken.id)
+
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+      user: user._id
+    })
+
     if (blog.title === undefined && blog.url === undefined) {
       response.status(400).end()
     } else {
       if (blog.likes === undefined) {
         blog.likes = 0
       }
-      const newBlog = await blog.save()
-      response.status(201).json(newBlog.toJSON())
+      const savedBlog = await blog.save()
+      user.blogs = user.blogs.concat(savedBlog._id)
+      await user.save()
+      response.status(201).json(savedBlog.toJSON())
     }
   } catch(exception) {
     next(exception)
