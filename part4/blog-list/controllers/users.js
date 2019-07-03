05061f7fdc45e81531c6c7f5e199574dfc7f2efa
Simon Mai <maismin@gmail.com>
2019-07-03 10:40:33 -0400

    Read all users through a HTTP GET request to /api/users

--- part4/blog-list/controllers/users.js
+++ part4/blog-list/controllers/users.js
@@ -2,6 +2,15 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+usersRouter.get('/', async (request, response, next) => {
+  try {
+    const users = await User.find({})
+    response.json(users.map(user => user.toJSON()))
+  } catch(exception) {
+    next(exception)
+  }
+})
+
 usersRouter.post('/', async (request, response, next) => {
   try {
     const body = request.body
