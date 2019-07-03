68b8dff0b8d55e42f21b57e6136164352eecf06d
Simon Mai <maismin@gmail.com>
2019-07-03 11:18:17 -0400

    Send correct HTML status code when a new user is successfully created

--- part4/blog-list/controllers/users.js
+++ part4/blog-list/controllers/users.js
@@ -34,7 +34,7 @@ usersRouter.post('/', async (request, response, next) => {
 
     const savedUser = await user.save()
 
-    response.json(savedUser)
+    response.status(201).json(savedUser.toJSON())
   } catch(exception) {
     next(exception)
   }
