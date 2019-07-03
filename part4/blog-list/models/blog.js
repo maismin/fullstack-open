cd9ca0b2665c055d1bc5e9b0d8fc9233cfa13d3c
Simon Mai <maismin@gmail.com>
2019-07-03 12:21:38 -0400

    Fix lint errors

--- part4/blog-list/models/blog.js
+++ part4/blog-list/models/blog.js
@@ -7,9 +7,9 @@ const blogSchema = mongoose.Schema({
   author: String,
   url: String,
   likes: Number,
-  user: {    
-    type: mongoose.Schema.Types.ObjectId,    
-    ref: 'User'  
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
   }
 })
 
