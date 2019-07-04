f01d0bc3989abb3b7cbeef0aa29d88cdb12134f2
Simon Mai <maismin@gmail.com>
2019-07-04 17:33:15 -0400

    Fix blog DELETE request to check token error and update blog POST request to reference user

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -42,10 +42,17 @@ blogsRouter.post('/', async (request, response, next) => {
       if (blog.likes === undefined) {
         blog.likes = 0
       }
-      const savedBlog = await blog.save()
-      user.blogs = user.blogs.concat(savedBlog._id)
-      await user.save()
-      response.status(201).json(savedBlog.toJSON())
+      blog.save()
+        .then(returnedBlog => {
+          returnedBlog
+            .populate('user', { username: 1, name: 1 })
+            .execPopulate()
+            .then(async savedBlog => {
+              user.blogs = user.blogs.concat(savedBlog._id)
+              await user.save()
+              response.status(201).json(savedBlog.toJSON())
+            })
+        })     
     }
   } catch(exception) {
     next(exception)
@@ -75,10 +82,16 @@ blogsRouter.put('/:id', async (request, response, next) => {
 
 blogsRouter.delete('/:id', async (request, response, next) => {
   try {
-    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    const token = request.token
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({
+        error: 'token missing or invalid'
+      })
+    }
+
     const user = await User.findById(decodedToken.id)
     const blog = await Blog.findById(request.params.id)
-
     if (!blog) {
       response.status(404).end()
     }
@@ -86,7 +99,8 @@ blogsRouter.delete('/:id', async (request, response, next) => {
     if (user._id.toString() === blog.user.toString()) {
       await Blog.findByIdAndRemove(request.params.id)
       //remove blogid from user
-      user.blogs = user.blogs.filter(blog => blog.toString() !== request.params.id)
+      console.log(user.blogs)
+      user.blogs = user.blogs.filter(blogID => blogID.toString() !== request.params.id)
       await User.findByIdAndUpdate(decodedToken.id, user)
       response.status(204).end()
     } else {
