3e63d6ee456b4793bee0b7fe617c2a9e18e861b8
Simon Mai <maismin@gmail.com>
2019-07-03 21:06:38 -0400

    Implement login functionality to frontend and display blogs when login is successful

--- part5/blog-list-frontend/src/App.js
+++ part5/blog-list-frontend/src/App.js
@@ -1,8 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
 
-function App() {
+
+const App = () => {
+  const [ blogs, setBlogs ] = useState([])
+  const [ username, setUsername ] = useState('')
+  const [ password, setPassword ] = useState('')
+  const [ user, setUser ] = useState(null)
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+          />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+          />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+      const blogs = await blogService.getAll()
+      console.log(blogs)
+      setBlogs(blogs)
+    } catch(exception) {
+      console.log(exception)
+    }
+  }
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        {loginForm()}
+      </div>
+    )
+  }
+
   return (
     <div>
+      <h2>Blogs</h2>
+      <p>{user.name} logged in</p>
+      {blogs.map(blog => <Blog key={blog.id} blog={blog}/>)}
     </div>
   );
 }
