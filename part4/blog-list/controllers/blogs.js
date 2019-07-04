9ffd3e09e4680a08785cb3b7c06969489aaf135f
Simon Mai <maismin@gmail.com>
2019-07-04 14:32:17 -0400

    Change Blog PUT request to populate user info

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -59,11 +59,14 @@ blogsRouter.put('/:id', async (request, response, next) => {
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes
+    likes: body.likes,
+    user: body.user
   }
 
   try {
-    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    const updatedBlog = await Blog
+      .findByIdAndUpdate(request.params.id, blog, { new: true })
+      .populate('user', { username: 1, name: 1 })
     response.json(updatedBlog.toJSON())
   } catch(exception) {
     next(exception)
