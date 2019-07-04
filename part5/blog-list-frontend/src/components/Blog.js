3a5c3856c511e398706606490e9e5209eadbf7e4
Simon Mai <maismin@gmail.com>
2019-07-04 13:51:51 -0400

    Modify Blog component to toggle info about a blog post when its name is clicked

--- part5/blog-list-frontend/src/components/Blog.js
+++ part5/blog-list-frontend/src/components/Blog.js
@@ -1,8 +1,32 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [ showInfo, setShowInfo ] = useState(false)
+
+  const displayInfo = { display: showInfo ? '' : 'none'}
+  const toggleShowInfo = () => {
+    setShowInfo(!showInfo)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div onClick={toggleShowInfo}>
+        {blog.title} {blog.author}
+      </div>
+      <div style={displayInfo}>
+        <a href={blog.url} target='_blank' rel='noopener noreferrer'>{blog.url}</a> <br/>
+        {blog.likes} likes <button>like</button> <br/>
+        added by {blog.user.name}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
