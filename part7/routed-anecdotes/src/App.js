45e4752eebbe181e8ccad1b8051a06a822b65a1c
Simon Mai <maismin@gmail.com>
2019-07-09 11:20:01 -0400

    Add react-router to app so that clicking links in the menu component changes the view

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,4 +1,8 @@
 import React, { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Route, Link, Redirect, withRouter
+} from 'react-router-dom'
 
 const Menu = () => {
   const padding = {
@@ -6,9 +10,9 @@ const Menu = () => {
   }
   return (
     <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
+      <Link style={padding} to="/">anecdotes</Link>
+      <Link style={padding} to="/create">create new</Link>
+      <Link style={padding} to="/about">about</Link>
     </div>
   )
 }
@@ -17,7 +21,11 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => 
+        <li key={anecdote.id} >
+          <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+        </li>
+      )}
     </ul>
   </div>
 )
@@ -124,11 +132,23 @@ const App = () => {
 
   return (
     <div>
-      <h1>Software anecdotes</h1>
-      <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
+      <Router>
+        <div>
+          <h1>Software anecdotes</h1>
+          <div>
+            <Menu />
+            <Route exact path="/" render={() =>
+              <AnecdoteList anecdotes={anecdotes} />
+            } />
+            <Route exact path="/create" render={() =>
+              <CreateNew addNew={addNew} />
+            } />
+            <Route exact path="/about" render={() =>
+              <About />
+            } />
+          </div>
+        </div>
+      </Router>
       <Footer />
     </div>
   )
