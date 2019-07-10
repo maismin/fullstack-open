04fc2ac8b06a1a1c691decba04f8801359e550b9
Simon Mai <maismin@gmail.com>
2019-07-10 12:19:06 -0400

    Add notification state to store

--- part7/blog-list-frontend-refactor/src/store.js
+++ part7/blog-list-frontend-refactor/src/store.js
@@ -4,10 +4,12 @@ import thunk from 'redux-thunk';
 
 import blogReducer from './reducers/blogReducer'
 import userReducer from './reducers/userReducer'
+import notificationReducer from './reducers/notificationReducer'
 
 const reducer = combineReducers({
   blogs: blogReducer,
-  user: userReducer
+  user: userReducer,
+  notification: notificationReducer
 })
 
 const store = createStore(
