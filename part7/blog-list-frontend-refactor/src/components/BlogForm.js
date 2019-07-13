092a376d7f7c417724a8421e5c7e4e25cb910a04
Simon Mai <maismin@gmail.com>
2019-07-13 14:28:27 -0400

    Update BlogForm component to use semantic-ui and be stateful

--- part7/blog-list-frontend-refactor/src/components/BlogForm.js
+++ part7/blog-list-frontend-refactor/src/components/BlogForm.js
@@ -2,9 +2,18 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { addBlog } from '../reducers/blogReducer'
 import { setMessage } from '../reducers/notificationReducer'
+import { useField } from '../hooks'
+import {
+  Button,
+  Form,
+  Header
+} from 'semantic-ui-react'
 
 const BlogForm = (props) => {
-  let formRef = React.createRef()
+  const title = useField('text')
+  const author = useField('text')
+  const url = useField('text')
+
   const handleAddBlog = async event => {
     event.preventDefault()
     const newBlog = {
@@ -13,30 +22,22 @@ const BlogForm = (props) => {
       url: event.target.url.value
     }
     props.addBlog(newBlog)
+    title.reset()
+    author.reset()
+    url.reset()
     props.setMessage(`a new blog '${newBlog.title}' by ${newBlog.author} added`, 'success', 3000)
-    formRef.reset()
   }
 
   return (
     <div>
-      <h2>Create New</h2>
-      <form onSubmit={handleAddBlog} ref={(el) => formRef = el}>
-        <div>
-          title:
-          <input name="title"/>
-        </div>
+      <Header as='h2'>Create New</Header>
+      <Form onSubmit={handleAddBlog}>
+        <Form.Input fluid placeholder='Title' name='title' {...title} reset=''/>
+        <Form.Input fluid placeholder='Author' name='author' {...author} reset=''/>
+        <Form.Input fluid placeholder='URL' name='url' {...url} reset=''/>
 
-        <div>
-          author:
-          <input name="author"/>
-        </div>
-
-        <div>
-          url:
-          <input name="url"/>
-        </div>
-        <button type="submit">create</button>
-      </form>
+        <Button type="submit">Create</Button>
+      </Form>
     </div>
   )
 }
