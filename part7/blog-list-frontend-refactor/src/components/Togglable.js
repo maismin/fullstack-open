07756f277ac75dce6934e0da3eaeb6fb2f9ed53a
Simon Mai <maismin@gmail.com>
2019-07-13 14:30:59 -0400

    Update Togglable component to use semantic-ui

--- part7/blog-list-frontend-refactor/src/components/Togglable.js
+++ part7/blog-list-frontend-refactor/src/components/Togglable.js
@@ -1,4 +1,7 @@
 import React, { useState, useImperativeHandle } from 'react'
+import {
+  Button
+} from 'semantic-ui-react'
 
 const Togglable = React.forwardRef((props, ref) => {
   const [ visible, setVisible ] = useState(false)
@@ -19,12 +22,12 @@ const Togglable = React.forwardRef((props, ref) => {
   return (
     <div>
       <div style={hideWhenVisible}>
-        <button onClick={toggleVisibility}>{props.buttonLabel}</button>
+        <Button onClick={toggleVisibility}>{props.buttonLabel}</Button>
       </div>
 
       <div style={showWhenVisible}>
         {props.children}
-        <button onClick={toggleVisibility}>cancel</button>
+        <Button onClick={toggleVisibility}>cancel</Button>
       </div>
     </div>
   )
